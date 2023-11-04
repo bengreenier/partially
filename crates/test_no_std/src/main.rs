a863c23a6a309dd2f4479ca31ffe9d298f0d21f2
bengreenier <ben@bengreenier.com>
2023-11-04 12:20:25 -0700

    feat(no_std): Add support

--- crates/test_no_std/src/main.rs
+++ crates/test_no_std/src/main.rs
@@ -2,11 +2,6 @@
 #![feature(lang_items, start)]
 #![no_std]
 
-#[start]
-fn start(_argc: isize, _argv: *const *const u8) -> isize {
-    0
-}
-
 #[lang = "eh_personality"]
 #[no_mangle]
 pub extern "C" fn rust_eh_personality() {}
@@ -25,4 +20,14 @@ use partially::Partial;
 #[derive(Partial)]
 struct Test {
     data: i32,
+}
+
+#[start]
+fn start(_argc: isize, _argv: *const *const u8) -> isize {
+    let mut base = Test { data: 1 };
+    let partial = PartialTest { data: Some(2) };
+
+    assert!(base.apply_some(partial));
+
+    0
 }
